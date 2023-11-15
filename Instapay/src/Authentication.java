1ac78d09b8592c92a5fa234f43151397a16ff53a
noureen <noureensaad086@gmail.com>
2023-11-15 18:43:02 +0200

    Authentication Update

--- Instapay/src/Authentication.java
+++ Instapay/src/Authentication.java
@@ -1,19 +1,26 @@
 public class Authentication {
-
-	/**
-	 * 
-	 * @param username
-	 * @param pass
-	 */
 	public boolean login(String username, String pass) {
-
-	return true;
-		
+		if(Instapay.db.checkBUser(username)){
+			if(Instapay.db.getUser(username).getPassword().equals(pass)){
+				Instapay.user = Instapay.db.getUser(username);
+				return true;
+			}
+			else{
+				Instapay.response.put("error message","Wrong password");
+				return false;
+			}
+		} else if(Instapay.db.checkWUser(username)){
+			if(Instapay.db.getUser(username).getPassword().equals(pass)){
+				Instapay.user = Instapay.db.getUser(username);
+				return true;
+			}
+			else{
+				Instapay.response.put("error message","Wrong password");
+				return false;
+			}
+		} else {
+			Instapay.response.put("error message","User not found");
+			return false;
+		}
 	}
-
-	public void operation() {
-		// TODO - implement Authentication.operation
-		throw new UnsupportedOperationException();
-	}
-
 }
