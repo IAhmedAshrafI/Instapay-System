7799f1f7daf8cccf2c05ac71b4dd194c8d2adebb
Seif <SE.SeifIbrahim@gmail.com>
2023-11-15 17:13:26 +0200

    made use of the Registeration and cleaned the view

--- Instapay/src/DB.java
+++ Instapay/src/DB.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.util.HashMap;
 
 public class DB {
--- Instapay/src/Instapay.java
+++ Instapay/src/Instapay.java
@@ -86,242 +86,131 @@ public class Instapay {
 	}
 
 	private void registerBankAccView(Scanner scanner) {
-		String name, password, BankNum, phoneNumber, phoneNumber_OTP, OTP_Code;
-		Validation validation = new Validation();
-		BankValidation bankValidation = new BankValidation();
+		String username, password, phoneNum, bankNum;
 		System.out.print("Username: ");
-		name = scanner.next();
-
-		while(bankValidation.validateBankUser(name))
-		{
-			System.out.println("This username is already exist.");
-			System.out.print("Username: ");
-			name = scanner.next();
-		}
-
-		while (!validation.isValidUsername(name)) {
-			System.out.println("Invalid Username.");
-			System.out.print("Username: ");
-			name = scanner.next();
-		}
+		username = scanner.next();
 
 		System.out.print("Password: ");
 		password = scanner.next();
-
-		while (!validation.isValidPassword(password)) {
-			System.out.println("Invalid password.");
-			System.out.print("Password: ");
-			password = scanner.next();
-		}
-
+		
 		System.out.print("Phone number: ");
-		phoneNumber = scanner.next();
-
-		while (!validation.isValidPhoneNumber(phoneNumber)) {
-
-			System.out.println("Invalid Phone number.");
-			System.out.print("Phone number: ");
-			phoneNumber_OTP = scanner.next();
-		}
+		phoneNum = scanner.next();
 
 		System.out.println("Bank number: ");
-		BankNum = scanner.next();
+		bankNum = scanner.next();
 
-		while(!bankValidation.validateBankData(BankNum))
-		{
-			System.out.println("There is no bank number related with your account.");
-			System.out.println("Bank number: ");
-			BankNum = scanner.next();
-		}
 
-		Registeration SignupBankAcc = new SignupBankAcc();
 
-		if (SignupBankAcc.signup()) {
+		SignupBankAcc signupBankAcc = new SignupBankAcc(bankNum, db.bank);
 
-			if (validation.validateBasicAcc(name, password, phoneNumber)) {
-				System.out.println("Verify your account with phone number");
-				System.out.print("Phone number: ");
-				phoneNumber_OTP = scanner.next();
-				while (!validation.isValidPhoneNumber(phoneNumber_OTP)) {
+		if (signupBankAcc.signup(username,password,phoneNum)) {
+			if (Registeration.otp.send(phoneNum)) {
+				System.out.print("Enter OTP: ");
+				String OTP_Code = scanner.next();
 
-					System.out.println("Invalid Phone number.");
-					System.out.print("Phone number: ");
-					phoneNumber_OTP = scanner.next();
-				}
-
-				if (Registeration.otp.send(phoneNumber_OTP)) {
-					System.out.println("Sending OTP...");
-					System.out.println(Registeration.otp.generate()
-							+ " is your Instapay OTP. Do not share it with anyone.");
-
+				while (!Registeration.otp.verify(OTP_Code)) {
+					System.out.println("Invalid OTP.");
 					System.out.print("Enter OTP: ");
 					OTP_Code = scanner.next();
-
-					while (!Registeration.otp.verify(OTP_Code)) {
-						System.out.println("Invalid OTP.");
-						System.out.print("Enter OTP: ");
-						OTP_Code = scanner.next();
-					}
-
-					if (Registeration.otp.verify(OTP_Code)) {
-
-						DB db = new DB();
-						BankAccUser bankUser = new BankAccUser();
-
-						bankUser.setUsername(name);
-						bankUser.setPassword(password);
-						bankUser.setPhoneNum(phoneNumber_OTP);
-						BankAcc bankAcc = new BankAcc();
-						bankAcc.setBank(db.bank);
-						bankUser.setBankAcc(bankAcc);
-
-						db.addBankUserToDB(bankUser);
-
-						System.out.println("Account created successfully.");
-					}
-
 				}
 
+				if (Registeration.otp.verify(OTP_Code)) {
+					BankAccUser bankUser = new BankAccUser();
+
+					bankUser.setUsername(username);
+					bankUser.setPassword(password);
+					bankUser.setPhoneNum(phoneNum);
+					BankAcc bankAcc = new BankAcc();
+					bankAcc.setBank(db.bank);
+					bankUser.setBankAcc(bankAcc);
+
+					db.addBankUserToDB(bankUser);
+
+					System.out.println("Account created successfully.");
+				}
 			}
+		} else {
+			System.out.println(response.get("error message"));
 		}
+		
 	}
 
 	private void registerWalletAccView(Scanner scanner) {
-		String name, password, phoneNumber, phoneNumber_OTP, OTP_Code;
-		Validation validation = new Validation();
-		WalletValidation walletValidation = new WalletValidation();
+
+		String username, password, phoneNum;
+		WalletProvider walletProvider = null;
 		System.out.print("Username: ");
-		name = scanner.next();
+		username = scanner.next();
 
-		while(walletValidation.validateWalletUser(name))
-		{
-			System.out.println("This username is already exist.");
-				System.out.print("Username: ");
-				name = scanner.next();
-		}
-
-			while (!validation.isValidUsername(name)) {
-				System.out.println("Invalid Username.");
-				System.out.print("Username: ");
-				name = scanner.next();
-			}
-
-			System.out.print("Password: ");
-			password = scanner.next();
-
-			while (!validation.isValidPassword(password)) {
-				System.out.println("Invalid password.");
-				System.out.print("Password: ");
-				password = scanner.next();
-			}
-
-			System.out.print("Phone number: ");
-			phoneNumber = scanner.next();
-			
-
-			while (!validation.isValidPhoneNumber(phoneNumber)) {
-
-				System.out.println("Invalid Phone number.");
-				System.out.print("Phone number: ");
-				phoneNumber_OTP = scanner.next();
-			}
-
-			System.out.println("Choose your Wallet");
-			System.out.println("1. CIB");
-			System.out.println("2. Vodafone Cash");
-			System.out.println("3. Fawry");
-			System.out.println("4. Back");
-		int ChooseWallet = scanner.nextInt();
-
-		switch (ChooseWallet) {
-			case 1:
-			CIB cib = new CIB();
-			if (walletValidation.validateWalletData(phoneNumber, cib)) {
-
-				System.out.println("This phone number is existed in cib.");
-
-			}else{
-				System.out.println("This phone number is NOT existed in cib.");
-			}
-				
-				break;
-
-				case 2:
-				VodafoneCash vc = new VodafoneCash();
-				if (walletValidation.validateWalletData(phoneNumber,vc)){
-					System.out.println("This phone number is existed in vodafone cash.");
-					} else {
-						System.out.println("This phone number is NOT existed in vodafone cash.");
-						}
-						break;
-
-				case 3:
-				Fawry fw = new Fawry();
-				if (walletValidation.validateWalletData(phoneNumber, fw)){
-					System.out.println("This phone number is existed in fawry.");
-				}else
-				{
-					System.out.println("This phone number is NOT existed in fawry.");
-				}
-
-				case 4:
-
-				System.out.println("holaa");
-				break;
+		System.out.print("Password: ");
+		password = scanner.next();
 		
+		System.out.print("Phone number: ");
+		phoneNum = scanner.next();
+
+		System.out.println("Choose your Wallet");
+		System.out.println("1. CIB");
+		System.out.println("2. Vodafone Cash");
+		System.out.println("3. Fawry");
+		int chooseWallet;
+
+		do {
+            System.out.println("Choose a wallet provider:");
+            System.out.println("1. CIB");
+            System.out.println("2. Vodafone Cash");
+            System.out.println("3. Fawry");
+            
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input.");
+                scanner.next();
+            }
+            
+            chooseWallet = scanner.nextInt();
+        } while (chooseWallet < 1 || chooseWallet > 3);
+
+		switch (chooseWallet) {
+			case 1:
+			walletProvider = db.cib;
+			case 2:
+			walletProvider = db.vodafoneCash;
+			case 3:
+			walletProvider = db.fawry;
 			default:
 				break;
 		}
 
-			Registeration SignupWalletAcc = new SignupWalletAcc();
+		SignupWalletAcc signupWalletAcc = new SignupWalletAcc(phoneNum, walletProvider);
 
-			if (SignupWalletAcc.signup()) {
+		if (signupWalletAcc.signup(username,password,phoneNum)) {
+			if (Registeration.otp.send(phoneNum)) {
+				System.out.print("Enter OTP: ");
+				String OTP_Code = scanner.next();
 
-				if (validation.validateBasicAcc(name, password, phoneNumber)) {
-					System.out.println("Verify your account with phone number");
-					System.out.print("Phone number: ");
-					phoneNumber_OTP = scanner.next();
-					while (!validation.isValidPhoneNumber(phoneNumber_OTP)) {
+				while (!Registeration.otp.verify(OTP_Code)) {
+					System.out.println("Invalid OTP.");
+					System.out.print("Enter OTP: ");
+					OTP_Code = scanner.next();
+				}
 
-						System.out.println("Invalid Phone number.");
-						System.out.print("Phone number: ");
-						phoneNumber_OTP = scanner.next();
-					}
+				if (Registeration.otp.verify(OTP_Code)) {
+					WalletAccUser walletUser = new WalletAccUser();
 
-					if (Registeration.otp.send(phoneNumber_OTP)) {
-						System.out.println("Sending OTP...");
-						System.out.println(Registeration.otp.generate()
-								+ " is your Instapay OTP. Do not share it with anyone.");
+					walletUser.setUsername(username);
+					walletUser.setPassword(password);
+					walletUser.setPhoneNum(phoneNum);
+					WalletAcc walletAcc = new WalletAcc();
+					walletAcc.setWalletProvider(walletProvider);
 
-						System.out.print("Enter OTP: ");
-						OTP_Code = scanner.next();
+					db.addWalletUserToDB(walletUser);
 
-						while (!Registeration.otp.verify(OTP_Code)) {
-							System.out.println("Invalid OTP.");
-							System.out.print("Enter OTP: ");
-							OTP_Code = scanner.next();
-						}
-
-						if (Registeration.otp.verify(OTP_Code)) {
-
-							DB db = new DB();
-
-							WalletAccUser WalletUser = new WalletAccUser();
-
-							WalletUser.setUsername(name);
-							WalletUser.setPassword(password);
-							WalletUser.setPhoneNum(phoneNumber);
-
-							db.addWalletUserToDB(WalletUser);
-
-							System.out.println("Account created successfully.");
-						}
-
-					}
-
+					System.out.println("Account created successfully.");
 				}
 			}
+		} else {
+			System.out.println(response.get("error message"));
+		}
 	}
+
 
 	private void AuthenticationView(Scanner scanner) {
 		String name, password;
